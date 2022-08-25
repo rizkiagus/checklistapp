911ca61cf09c83f00e70621536bf748ac1e7fe0f
rizkiagus <rizki.setiawanagus@gmail.com>
2022-08-25 17:48:49 +0700

    fix: register

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -4,10 +4,28 @@
       <h2 class="text-center mt-12">Register</h2>
       <v-col cols="4"></v-col>
       <v-col cols="4" class="mx-auto">
-        <v-text-field label="Email" outlined dense></v-text-field>
-        <v-text-field label="Username" outlined dense></v-text-field>
-        <v-text-field label="Password" outlined dense></v-text-field>
-        <v-btn class="primary mb-3" width="100%" depressed>register</v-btn>
+        <v-text-field
+          label="Email"
+          outlined
+          dense
+          v-model="email"
+        ></v-text-field>
+        <v-text-field
+          label="Username"
+          outlined
+          dense
+          v-model="username"
+        ></v-text-field>
+        <v-text-field
+          label="Password"
+          outlined
+          dense
+          type="password"
+          v-model="password"
+        ></v-text-field>
+        <v-btn class="primary mb-3" width="100%" depressed @click="register"
+          >register</v-btn
+        >
         <span class="me-5">Belum Mempunyai Akun?</span>
         <span
           class="primary--text font-weight-bold rgs"
@@ -27,18 +45,21 @@ export default {
   setup() {},
   data() {
     return {
-      user: {
-        email: "",
-        password: "",
-        username: "",
-      },
+      email: "",
+      password: "",
+      username: "",
     };
   },
   methods: {
     register() {
-      Axios.post("http://94.74.86.174:8080/api/register", this.user)
+      Axios.post("http://94.74.86.174:8080/api/register", {
+        email: this.email,
+        password: this.password,
+        username: this.username,
+      })
         .then((response) => {
           console.log(response);
+          this.$router.go("/login");
         })
         .catch((error) => console.log(error));
     },
